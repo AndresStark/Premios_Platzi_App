d81bd250c9cc016416b1d872462cc27744afdd16
Cloud <andres.sanchez.cloud@gmail.com>
2022-12-05 16:29:30 -0600

    Elevando error 404, todo funciona

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Estás en la página principal de Premios Platzi App")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+    # question = get_list_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")    
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 
 def results(request, question_id):
