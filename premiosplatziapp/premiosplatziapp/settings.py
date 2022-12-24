8d9365ddd5c20b6b8f03fbaa18cc112ef0240cb3
Cloud <andres.sanchez.cloud@gmail.com>
2022-12-23 20:32:55 -0600

    Agregué una buena estructura HTML a Polls

--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_^nsi7u-69nl00pmema!nqjq#k@w$(9_uuq-h&h7n2*t_h4t%)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
